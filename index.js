60d4e3e720cba99397cd59f2ba3e40e5487bf0b3
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-01 13:35:59 -0700

    added file structure as per directions

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+// Requires the library files you will be writing (input, notes)
+// Instantiates an instance of an "Input" parser module
+// Parses the command line input and returns the command and data
+// Passes the command to the Notes library, which executes the command
+
+console.log('hi'); 
