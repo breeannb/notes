569e6613cb37e6980a2227e0e8632b2fc5c13086
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-01 15:44:48 -0700

    wrote valid tset with Ryan

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-
+const { parse } = require('./lib/input');
 // Requires the library files you will be writing (input, notes)
 // Instantiates an instance of an "Input" parser module
     // Parses the command line input and returns the command and data
 // Passes the command to the Notes library, which executes the command
 
-console.log(process.argv); 
+console.log(parse(process.argv)); 
