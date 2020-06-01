f4ad2f804789d7499c772bea98ff0e2f61398df0
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-01 14:43:47 -0700

    created skip function to pass over node index in command line for parse

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Requires the library files you will be writing (input, notes)
 // Instantiates an instance of an "Input" parser module
-// Parses the command line input and returns the command and data
+    // Parses the command line input and returns the command and data
 // Passes the command to the Notes library, which executes the command
 
-console.log('hi'); 
+console.log(process.argv); 
