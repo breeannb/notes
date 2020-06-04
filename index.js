4e14a58b94d1737b6f3862986e836b9e10809f72
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-04 07:53:44 -0700

    uncommented some things from indexjs and tried to clean up extra lines/comments

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
-// const { parse, valid } = require('./lib/input');
-// const { execute } = require('./lib/notes');
-
-// const action = parse(process.argv);
-// const isValid = valid(action); 
-
-// if(isValid);
-// else console.log('invalid command');
-
-
-// const express = require('express'); 
-// const app = express(); 
 const mongoose = require('mongoose');
 const Note = require('./lib/models/Notes.js'); 
+const { Input } = require('./lib/Input');
+let parsedArray = new Input(process.argv); 
+let response; 
+console.log(parsedArray.isValid()) 
 
-mongoose.connect('mongodb://localhost:27017/play', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+if(parsedArray.isValid()) {
+    response = Note.execute(parsedArray); 
+} else {
+    console.log('try again please')
+}
 
+const action = parse(process.argv);
+const isValid = valid(action); 
+
+// mongoose.connect('mongodb://localhost:27017/notelist', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
