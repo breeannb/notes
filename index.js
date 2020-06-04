cc8aa9c1d8b74340129be30a456bda4b623c89b7
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-04 06:53:39 -0700

    list test passes

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 
-const { parse, valid } = require('./lib/input');
-const { execute } = require('./lib/notes');
+// const { parse, valid } = require('./lib/input');
+// const { execute } = require('./lib/notes');
 
-const action = parse(process.argv);
-const isValid = valid(action); 
+// const action = parse(process.argv);
+// const isValid = valid(action); 
 
-if(isValid);
-else console.log('invalid command');
+// if(isValid);
+// else console.log('invalid command');
+
+
+// const express = require('express'); 
+// const app = express(); 
+const mongoose = require('mongoose');
+const Note = require('./lib/models/Notes.js'); 
+
+mongoose.connect('mongodb://localhost:27017/play', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
